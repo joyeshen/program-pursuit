6bdc5cf18392da4d362331b8b33b47a1d6c95e34
Joye Shen <joyeshen05@gmail.com>
2023-08-02 11:40:56 -0700

    Initial Commit

---
+++ program-pursuit/program_pursuitApp.swift
@@ -0,0 +1,20 @@
+//
+//  program_pursuitApp.swift
+//  program-pursuit
+//
+//  Created by Joye Shen on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct program_pursuitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
