a1c40dd959ebc351396532441ac5e2df277a2fe1
adela <ruizadela126@gmail.com>
2023-08-03 12:01:43 -0700

    I tried to finish putting in links for the scholarship tabs

--- program-pursuit/Scholarship.swift
+++ program-pursuit/Scholarship.swift
@@ -86,12 +86,51 @@ struct Scholarship: View {
                                 .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
                             
                             VStack {
-                                Link("Visit Questbridge : National College Match", destination: URL(string:"https://www.scienceambassadorscholarship.org/")!)
+                                Link("Visit Questbridge : National College Match", destination: URL(string:"https://www.questbridge.org/high-school-students/national-college-match")!)
                                     .font(.headline)
                                     .fontWeight(.semibold)
                                 
                                 HStack {
-                                    Text("Full tuition for women in stem")
+                                    Text("Full tuition for partnered colleges")
+                                    NavigationLink(destination: ScienceAmbassador()) {
+                                        Text("Read More!")
+                                            .font(.callout)
+                                            .fontWeight(.black)
+                                            .foregroundColor(Color(hue: 0.872, saturation: 0.911, brightness: 0.767))
+                                    }
+                                }
+                            }
+                            
+                           
+                            
+                            
+                            
+                
+                            
+                            
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        ZStack {
+                            
+                            Image("PinkBackground 1")
+                                .resizable(resizingMode: .stretch)
+                                .frame(width: 370.0, height: 100.0)
+                                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                            
+                            VStack {
+                                Link("Visit Book Lovers Scholars", destination: URL(string:"https://bold.org/scholarships/book-lovers-scholarship/")!)
+                                    .font(.headline)
+                                    .fontWeight(.semibold)
+                                
+                                HStack {
+                                    Text("$500 ")
                                     NavigationLink(destination: ScienceAmbassador()) {
                                         Text("Read More!")
                                             .font(.callout)
@@ -102,6 +141,7 @@ struct Scholarship: View {
                             }
                             
                             
+                            
                         }
                         
                         
@@ -118,37 +158,21 @@ struct Scholarship: View {
                                 .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
                             
                             VStack {
-                                Text("Link")
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text("Description")
-                                    .font(.body)
-                                    .fontWeight(.regular)
+                                Link("Visit  DeVaney Memorial Scholarship Fund", destination: URL(string:"https://bold.org/scholarships/barbara-j-devaney-memorial-scholarship-fund/")!)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                
+                                HStack {
+                                    Text("Amount given Varies ")
+                                    NavigationLink(destination: ScienceAmbassador()) {
+                                        Text("Read More!")
+                                            .font(.callout)
+                                            .fontWeight(.black)
+                                            .foregroundColor(Color(hue: 0.872, saturation: 0.911, brightness: 0.767))
+                                    }
+                                }
                             }
                             
-                            
-                        }
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        ZStack {
-                            
-                            Image("PinkBackground 1")
-                                .resizable(resizingMode: .stretch)
-                                .frame(width: 370.0, height: 100.0)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
-                            
-                            VStack {
-                                Text("Link")
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text("Description")
-                                    .font(.body)
-                                    .fontWeight(.regular)
                             }
                             
                             
@@ -171,14 +195,20 @@ struct Scholarship: View {
                                 .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
                             
                             VStack {
-                                Text("Link")
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text("Description")
-                                    .font(.body)
-                                    .fontWeight(.regular)
+                                Link("Visit Disney Super Fan Scholarship", destination: URL(string:"https://bold.org/scholarships/disney-super-fan-scholarship/")!)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                
+                                HStack {
+                                    Text("Amount given is $1,000 ")
+                                    NavigationLink(destination: ScienceAmbassador()) {
+                                        Text("Read More!")
+                                            .font(.callout)
+                                            .fontWeight(.black)
+                                            .foregroundColor(Color(hue: 0.872, saturation: 0.911, brightness: 0.767))
+                                    }
+                                }
                             }
-                            
                             
                         }
                         
@@ -199,50 +229,22 @@ struct Scholarship: View {
                                 .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
                             
                             VStack {
-                                Text("Link")
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text("Description")
-                                    .font(.body)
-                                    .fontWeight(.regular)
+                                Link("Visit moolahSPOT", destination: URL(string:"http://www.moolahspot.com/scholarship/index.cfm")!)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                
+                                HStack {
+                                    Text("Amount given is $1,000 ")
+                                    NavigationLink(destination: ScienceAmbassador()) {
+                                        Text("Read More!")
+                                            .font(.callout)
+                                            .fontWeight(.black)
+                                            .foregroundColor(Color(hue: 0.872, saturation: 0.911, brightness: 0.767))
+                                    }
+                                }
                             }
                             
-                            
                         }
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        ZStack {
-                            
-                            Image("PinkBackground 1")
-                                .resizable(resizingMode: .stretch)
-                                .frame(width: 370.0, height: 100.0)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
-                            
-                            VStack {
-                                Text("Link")
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text("Description")
-                                    .font(.body)
-                                    .fontWeight(.regular)
-                            }
-                            
-                            
-                        }
-                        
-                        
-                        
                         
                         
                         
@@ -264,12 +266,59 @@ struct Scholarship: View {
                                 .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
                             
                             VStack {
-                                Text("Link")
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text("Description")
-                                    .font(.body)
-                                    .fontWeight(.regular)
+                                Link("Visit Bryan Cameron Education Foundation", destination: URL(string:"https://www.bryancameroneducationfoundation.org/scholarship")!)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                
+                                HStack {
+                                    Text("Full Tution + covers fees & books")
+                                    NavigationLink(destination: ScienceAmbassador()) {
+                                        Text("Read More!")
+                                            .font(.callout)
+                                            .fontWeight(.black)
+                                            .foregroundColor(Color(hue: 0.872, saturation: 0.911, brightness: 0.767))
+                                    }
+                                }
+                            }
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        ZStack {
+                            
+                            Image("PinkBackground 1")
+                                .resizable(resizingMode: .stretch)
+                                .frame(width: 370.0, height: 100.0)
+                                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                            
+                            VStack {
+                                Link("Visit We the Future", destination: URL(string:"https://constitutingamerica.org/contest-categories/")!)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                
+                                HStack {
+                                    Text("Amount Varies")
+                                    NavigationLink(destination: ScienceAmbassador()) {
+                                        Text("Read More!")
+                                            .font(.callout)
+                                            .fontWeight(.black)
+                                            .foregroundColor(Color(hue: 0.872, saturation: 0.911, brightness: 0.767))
+                                    }
+                                }
                             }
                             
                             
@@ -309,7 +358,7 @@ struct Scholarship: View {
         
         
     }
-}
+
 
 struct Scholarship_Previews: PreviewProvider {
     static var previews: some View {
